96df434ce7801839435c627b43bd6d78a3edcaf6
markice25 <markice@live.com>
2020-08-17 21:59:37 -0400

    0817

--- Makefile
+++ Makefile
@@ -1,15 +1,26 @@
+CC = arm-linux-gnueabi-gcc
+CFLAGS = -static -c -O2 -nostdlib -fno-builtin -fno-stack-protector -fno-PIC -march=armv7-a -mfloat-abi=soft -std=c99 -Wall
+
+OBJS = obj/boot.o obj/start.o obj/printf.o obj/vm.o
+
 .PHONY: all clean qemu
 
 all: boot
 
-boot: obj/boot.o obj/start.o src/boot.ld
-	arm-linux-gnueabi-ld -static -T src/boot.ld -o boot obj/start.o obj/boot.o
+boot: $(OBJS) src/boot.ld
+	arm-linux-gnueabi-ld -static -T src/boot.ld -o $@ $(OBJS)
 
 obj/start.o: src/start.S
-	arm-linux-gnueabi-gcc -static -c -O2 -nostdlib -fno-builtin -fno-stack-protector -fno-PIC -march=armv7-a -mfloat-abi=soft -std=c99 -Wall -o obj/start.o src/start.S
+	$(CC) $(CFLAGS) -o $@ $<
 
-obj/boot.o: src/boot.c
-	arm-linux-gnueabi-gcc -static -c -O2 -nostdlib -fno-builtin -fno-stack-protector -fno-PIC -march=armv7-a -mfloat-abi=soft -std=c99 -Wall -o obj/boot.o src/boot.c
+obj/boot.o: src/boot.c src/*.h
+	$(CC) $(CFLAGS) -o $@ $<
+
+obj/printf.o: src/printf.c src/*.h
+	$(CC) $(CFLAGS) -o $@ $<
+
+obj/vm.o: src/vm.c src/*.h
+	$(CC) $(CFLAGS) -o $@ $<
 
 clean:
 	rm -f boot
--- src/boot.c
+++ src/boot.c
@@ -1,69 +1,40 @@
-typedef __builtin_va_list       va_list;
-#define va_start(ap, last)      __builtin_va_start(ap, last)
-#define va_arg(ap, type)        __builtin_va_arg(ap, type)
-#define va_end(ap)              __builtin_va_end(ap)
+#include "printf.h"
 
-#define BCM2835_BASE            (0x3f000000ul)
-#define PL011_BASE              (BCM2835_BASE + 0x201000ul)
-#define PL011_DR                (PL011_BASE)
-#define PL011_FR                (PL011_BASE + 0x18ul)
-
-static int raspi2_putchar(int ch)
-{
-    unsigned char ready = 0;
-    while (!ready) {
-        volatile unsigned char *addr = (void *)PL011_FR;
-        ready = !(*addr & 0x20);
-    }
-    
-    volatile unsigned char *addr = (void *)PL011_DR;
-    *addr = ch;
-    return 1;
-}
-
-static void print_hex(unsigned int num)
-{
-}
-
-int vprintk(const char *fmt, va_list args)
-{
-    unsigned int num = 0;
-    const char *s = fmt;
-    while (*s) {
-        char c = *s;
-        switch (c) {
-        case '%':
-            s++;
-            switch (*s) {
-            case 'd':
-                num = va_arg(args, unsigned int);
-                raspi2_putchar(num >> )
-            }
-            break;
-        default:
-            raspi2_putchar(c);
-            break;
-        }
-        s++;
-    }
-}
-
-int printk(const char *fmt, ...)
-{
-    int ret = 0;
-    
-    va_list args;
-    va_start(args, fmt);
-    ret = vprintk(fmt, args);
-    va_end(args);
-    
-    return ret;
-}
+extern void start_paging();
 
 int main()
 {
-    while (1) {
-        printk("test: %d\n", 100);
-    }
+/*    while (1) {*/
+/*        printk("test: %d\n", 1100100);*/
+/*    }*/
+/*    for (int i = 33; i < 130; i ++){*/
+/*    raspi2_putchar(i);*/ 
+/*    }*/
+/*    */
+/*    print_binary(1000);*/
+/*    printk("Print 1000 as binary:%b\n",1000);*/
+/*    printk("Print 1000 as hex:%h\n",1000);*/
+/*    printk("Print string: %s\n", "abcdefg");*/
+/*    printk("Print a as ASCII: %c\n",'a');*/
+/*    unsigned int q = 0;*/
+/*    unsigned int r = 0;*/
+/*    div32(48,3,&q,&r);*/
+/*    printk("48/3:quotient:%b\n, remainder:%b\n",q,r);*/
+/*    q = 0;*/
+/*    r = 0;*/
+/*    div32(100,100,&q,&r);*/
+/*    printk("100/100:quotient:%b\n, remainder:%b\n",q,r);*/
+/*    q = 0;*/
+/*    r = 0;*/
+/*    div32(100,1000,&q,&r);*/
+/*    printk("100/1000:quotient:%b\n, remainder:%b\n",q,r);*/
+/*    q = 0;*/
+/*    r = 0;*/
+/*    div32(923,9,&q,&r);*/
+/*    printk("923/9:quotient:%b\n, remainder:%b\n",q,r);*/
+    printk("print 234523 as decimal %d",234523);
+    printk("print a as char %c", 'a');
+    start_paging();
+    while (1);
 }
 
